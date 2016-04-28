04442f816fa8622ee29a1afafae454fc1f6bdf1c
José Naves <josenaves@gmail.com>
2016-04-28 15:29:40 -0300

    Remove input parameter

--- www/checkgps.js
+++ www/checkgps.js
@@ -1,7 +1,7 @@
 var exec = require('cordova/exec');
 
 module.exports = {
-    isGpsEnabled: function (name, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "CheckGps", "isGpsEnabled", [name]);
+    isGpsEnabled: function (successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "CheckGps", "isGpsEnabled", []);
     }
 };
